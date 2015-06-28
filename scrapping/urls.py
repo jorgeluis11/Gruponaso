f66860e911822ec99562612e5cf81d7bdba9fd88
jorgeluis11 <perez.jorge718@gmail.com>
2015-06-28 17:20:23 -0400

    changes and pep8 coding convention

--- scrapping/urls.py
+++ scrapping/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from .views import index
+from .views import index, pages
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', index),
+    url(r'^pages$', pages),
 )
